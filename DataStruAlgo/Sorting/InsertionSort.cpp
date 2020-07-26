e9b8076c587bb5f93d858ca8d422af628c4a0b2d
Goutham <Goutham@GOUTHI>
2020-07-26 13:42:08 +0530

    Code

--- DataStruAlgo/Sorting/InsertionSort.cpp
+++ DataStruAlgo/Sorting/InsertionSort.cpp
@@ -1,4 +1,22 @@
-#include <iostream>
+﻿#include <iostream>
+
+/*
+
+	Sort : Insersion Sort
+	Data Structure : Array
+
+	Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list. 
+	At each iteration, insertion sort removes one element from the input data, finds the location it belongs 
+	within the sorted list, and inserts it there. It repeats until no input elements remain.
+
+	Time and Space complexities :-
+	Worst-case performance			О(n2) comparisons, О(n2) swaps
+	Best-case performance			О(n) comparisons, О(1) swaps
+	Average performance				О(n2) comparisons, О(n2) swaps
+
+	Worst-case space complexity		O(n) total,O(1) auxiliary
+
+*/
 
 void InsertionSort(int p[],int len)
 {
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DataStrcAlgo
+
+https://www.hackerearth.com/practice/notes/sorting-and-searching-algorithms-time-complexities-cheat-sheet/
+
